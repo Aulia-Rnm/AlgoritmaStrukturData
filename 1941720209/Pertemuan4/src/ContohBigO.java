cc295f0d8bf70eb0bca805849390fd9a3646330b
Aulia_rnm <arnmqrt@gmail.com>
2020-02-27 11:45:28 +0700

    Pertemuan4

--- 1941720209/Pertemuan4/src/ContohBigO.java
+++ 1941720209/Pertemuan4/src/ContohBigO.java
@@ -9,5 +9,19 @@
  * @author Asus
  */
 public class ContohBigO {
-    
+    public static void contohBig0(int[]angka){
+        System.out.println("Pairs: ");
+        int n=angka.length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(angka[i]+"-"+angka[j]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(angka[i]+"-"+angka[j]);
+            }
+        }
+    }
 }
